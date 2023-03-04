5f9fa0996b6186aebee218a31a5c751c17d2f3d1
Sarmad Rehman <sarmadr02@gmail.com>
2023-03-04 17:25:37 +0500

    Passed the table through Hibernate-ORM utilities successfully

--- ProjectWithMaven/src/main/java/com/tut/App.java
+++ ProjectWithMaven/src/main/java/com/tut/App.java
@@ -1,6 +1,10 @@
 package com.tut;
 
+import java.util.Date;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -11,8 +15,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
-        System.out.println(factory);
+        System.out.println( "Project Started " );
+        Configuration cfg = new Configuration() ;
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+       // System.out.println(factory);
+        
+        //creating student
+        Student st = new Student () ;
+        st.setId(77);
+        st.setName("Sarmad Rehman");
+        st.setCity("Mianwali");
+        System.out.println(st);
+        
+        //creating object of address-class
+        Address ad = new Address() ;
+        ad.setStreet("NIaxi Street");
+        ad.setCity("Mianwali");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(123232);
+        
+        //Session configuration
+        Session session = factory.openSession() ;
+        Transaction tx =  session.beginTransaction() ;
+        session.save(st) ;
+        session.save(ad) ;
+        tx.commit();
+        session.close();
+        System.out.println("Done.....");
+        
     }
 }
